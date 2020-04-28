e38a395cb3ae893d236ce61424e5e3e22b0fb602
Gabriela Acosta <asm90@gmail.com>
2020-04-28 18:11:31 -0300

    unas cositas

--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import './Titulo.css';
 
 const Titulo = () => {
-  return <h1>Welcome Rock, Paper Scissors</h1>;
+  return <h1 className="Titulo-inicial">Welcome Rock, Paper Scissors</h1>;
 };
 
 export default Titulo;
