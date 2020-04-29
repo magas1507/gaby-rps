d37e9338e25ab6d45960cefc74401514f382c083
Hector Flores <Hector_Flores@eogresources.com>
2020-04-28 18:37:55 -0700

    OnClick en span como ejemplo

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './styles.css';
 
 // FUNCTIONAL COMPONENT
 const Title = (props) => {
+  const [text, setText] = useState('Hola');
+
+  const handleClickOnSpan = (event) => {
+    console.log('HOLA');
+    setText('HOLA GABY');
+  };
+
   return (
     <h1 className='title'>
       <span className='title-rock'>Rock</span>,{' '}
       <span className='title-paper'>Paper</span> &{' '}
       <span className='title-scissors'>Scissors</span>
+      <span onClick={handleClickOnSpan}>{text}</span>
     </h1>
   );
 };
