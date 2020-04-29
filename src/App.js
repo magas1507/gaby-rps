7c020cc1bb772d82d5c0c01fb245cdbbdd65059a
Hector Flores <Hector_Flores@eogresources.com>
2020-04-28 18:22:29 -0700

    Refator en componente title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import Title from './components/Title';
+import Button from './components/Botones';
+
 function App() {
-  return <div className='App'>Hola</div>;
+  return (
+    <div className='App'>
+      <Title />
+      <Button />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Title/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './styles.css';
+
+// FUNCTIONAL COMPONENT
+const Title = (props) => {
+  return (
+    <h1 className='title'>
+      <span className='title-rock'>Rock</span>,{' '}
+      <span className='title-paper'>Paper</span> &{' '}
+      <span className='title-scissors'>Scissors</span>
+    </h1>
+  );
+};
+
+export default Title;
