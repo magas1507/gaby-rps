e8b06cef487e052eb1a9041c8022a2f57deff44e
Gabriela Acosta <asm90@gmail.com>
2020-05-02 23:13:21 -0300

    validation-game

--- src/components/Options/index.js
+++ src/components/Options/index.js
@@ -1,23 +1,65 @@
-import React from 'react';
-import paper from './paper.png'
-import rock from './rock.png'
-import scissors from './scissors.png'
-import { useState } from 'react';
-import './style.css';
+import React from "react";
+import paper from "./paper.png";
+import rock from "./rock.png";
+import scissors from "./scissors.png";
+import { useState } from "react";
+import "./style.css";
 const Options = () => {
   //const [text, setText] = useState('Hola');
-  const imageClick = () => {
-    console.log('Click');
+
+  async function cpuAsync() {
+    let response = await fetch("https://smartplay.afiniti.com/v1/play/");
+
+    let data = await response.json();
+
+    return data;
   }
-  const isTrue = true;
+
+  const imageClick = (str) => {
+    cpuAsync()
+      .then((data) => {
+        console.log(str, data.nextBestMove);
+        let resolvetCpu = data.nextBestMove;
+
+        if (str === "P" && resolvetCpu === "S") {
+          console.log(`perdiste`);
+        } else if (str === "P" && resolvetCpu === "R") {
+          console.log("ganaste");
+        } else if (str === "S" && resolvetCpu === "R") {
+          console.log(`perdiste`);
+        } else if (str === "S" && resolvetCpu === "P") {
+          console.log(`Ganaste`);
+        } else if (str === "R" && resolvetCpu === "P") {
+          console.log("perdiste");
+        } else if (str === "R" && resolvetCpu === "S") {
+          console.log("ganaste");
+        } else {
+          console.log("Wrap");
+        }
+      })
+      .catch((reason) => console.log(reason.message));
+  };
+
   return (
     <div className="options-div">
-      
-      <img className='imagen-paper' src={paper} alt='imag'  onClick={() => imageClick()} ></img>
-      <img className='imagen-paper' src={rock} alt='imag'  onClick={() => imageClick()} ></img>
-      <img className='imagen-paper' src={scissors} alt='imag'  onClick={() => imageClick()} ></img>
-      {isTrue ? <h4>esto esd verdadero</h4> : <h5>Soy falso</h5>}
-      {isTrue && <h4>Soy verdadero</h4>}
+      <img
+        className="imagen-paper"
+        src={paper}
+        alt="imag"
+        onClick={() => imageClick("P")}
+      ></img>
+      <img
+        className="imagen-paper"
+        src={rock}
+        alt="imag"
+        onClick={() => imageClick("R")}
+      ></img>
+      <img
+        className="imagen-paper"
+        src={scissors}
+        alt="imag"
+        onClick={() => imageClick("S")}
+      ></img>
     </div>
   );
 };
--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -7,16 +7,16 @@ const Title = (props) => {
   const [text, setText] = useState('Hola');
 
   const handleClickOnSpan = (event) => {
-    console.log('HOLA');
-    setText('HOLA GABY');
+   
+ 
   };
 
   return (
     <h1 className='title'>
-      <span className='title-rock'>Rock</span>,{' '}
+      <span  className='title-rock'>Rock</span>,{' '}
       <span className='title-paper'>Paper</span> &{' '}
       <span className='title-scissors'>Scissors</span>
-      <span onClick={handleClickOnSpan}>{text}</span>
+      <span onClick={handleClickOnSpan()}>{text}</span>
     </h1>
   );
 };
