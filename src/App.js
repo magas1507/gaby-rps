24a8cfc8533ae6a74eaacba8fe9545e31cb5c96b
Hector Flores <Hector_Flores@eogresources.com>
2020-05-08 20:24:09 -0700

    Added states for players

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Title from './components/Title';
 import Options from './components/Options';
+import Results from './components/Results';
 
 function App() {
+  const [cpuPoints, setCpuPoints] = useState(0); // ESTADO const cpuPoints = 0;
+  const [playerPoints, setPlayerPoints] = useState(0); // ESTADO
+
+  const addOneCpu = () => {
+    setCpuPoints(cpuPoints + 1);
+  };
+  const addOnePlayer = () => {
+    setPlayerPoints(playerPoints + 1);
+  };
+
   return (
     <div className='App'>
       <Title />
-      <Options />
+      <Options addOneCpu={addOneCpu} addOnePlayer={addOnePlayer} />
+      <Results playerPoints={playerPoints} cpuPoints={cpuPoints} />
     </div>
   );
 }
--- src/components/Options/index.js
+++ src/components/Options/index.js
@@ -1,22 +1,17 @@
-import React from "react";
-import paper from "./paper.png";
-import rock from "./rock.png";
-import scissors from "./scissors.png";
-import { useState } from "react";
-import "./style.css";
-const Options = () => {
-  //const [text, setText] = useState('Hola');
+import React from 'react';
+import paper from './paper.png';
+import rock from './rock.png';
+import scissors from './scissors.png';
+import { useState } from 'react';
+import './style.css';
+
+const Options = ({ addOneCpu, addOnePlayer }) => {
+  // const { addOneCpu, addOnePlayer } = props;
 
   async function cpuAsync() {
-    try {
-      let response = await fetch("https://smartplay.afiniti.com/v1/play/");
-
-      let data = await response.json();
-
-      return data;
-    } catch (err) {
-      console.log(err.message);
-    }
+    let response = await fetch('https://smartplay.afiniti.com/v1/play/');
+    let data = await response.json();
+    return data;
   }
 
   const imageClick = async (str) => {
@@ -26,44 +21,50 @@ const Options = () => {
       console.log(str, data.nextBestMove);
       let resolvetCpu = data.nextBestMove;
 
-      if (str === "P" && resolvetCpu === "S") {
+      if (str === 'P' && resolvetCpu === 'S') {
         console.log(`perdiste`);
-      } else if (str === "P" && resolvetCpu === "R") {
-        console.log("ganaste");
-      } else if (str === "S" && resolvetCpu === "R") {
+        addOneCpu();
+      } else if (str === 'P' && resolvetCpu === 'R') {
+        console.log('ganaste');
+        addOnePlayer();
+      } else if (str === 'S' && resolvetCpu === 'R') {
         console.log(`perdiste`);
-      } else if (str === "S" && resolvetCpu === "P") {
+        addOneCpu();
+      } else if (str === 'S' && resolvetCpu === 'P') {
         console.log(`Ganaste`);
-      } else if (str === "R" && resolvetCpu === "P") {
-        console.log("perdiste");
-      } else if (str === "R" && resolvetCpu === "S") {
-        console.log("ganaste");
+        addOnePlayer();
+      } else if (str === 'R' && resolvetCpu === 'P') {
+        console.log('perdiste');
+        addOneCpu();
+      } else if (str === 'R' && resolvetCpu === 'S') {
+        console.log('ganaste');
+        addOnePlayer();
       } else {
-        console.log("Wrap");
+        console.log('Draw');
       }
     } catch (error) {
       console.log(error.message);
     }
   };
   return (
-    <div className="options-div">
+    <div className='options-div'>
       <img
-        className="imagen-paper"
+        className='imagen-paper'
         src={paper}
-        alt="imag"
-        onClick={() => imageClick("P")}
+        alt='imag'
+        onClick={() => imageClick('P')}
       ></img>
       <img
-        className="imagen-paper"
+        className='imagen-paper'
         src={rock}
-        alt="imag"
-        onClick={() => imageClick("R")}
+        alt='imag'
+        onClick={() => imageClick('R')}
       ></img>
       <img
-        className="imagen-paper"
+        className='imagen-paper'
         src={scissors}
-        alt="imag"
-        onClick={() => imageClick("S")}
+        alt='imag'
+        onClick={() => imageClick('S')}
       ></img>
     </div>
   );
